fa93f0c6350db1e3d9949c3d9dd8dc7aca5349dc
Budi Irawan <deerawan@gmail.com>
2022-02-15 21:56:05 +1100

    Replace faker lib with @faker-js/faker (#52)
    The original faker.js has been removed
    and not maintained anymore
    by the author, so the community
    creates the community version of faker.js
    
    Close #49

--- src/faker-factory.ts
+++ src/faker-factory.ts
@@ -1,94 +1,94 @@
 export function fakerFactory(locale: string) {
   switch (locale) {
     case 'az':
-      return import('faker/locale/az');
+      return import('@faker-js/faker/locale/az');
     case 'ar':
-      return import('faker/locale/ar');
+      return import('@faker-js/faker/locale/ar');
     case 'cz':
-      return import('faker/locale/cz');
+      return import('@faker-js/faker/locale/cz');
     case 'de':
-      return import('faker/locale/de');
+      return import('@faker-js/faker/locale/de');
     case 'de_AT':
-      return import('faker/locale/de_AT');
+      return import('@faker-js/faker/locale/de_AT');
     case 'de_CH':
-      return import('faker/locale/de_CH');
+      return import('@faker-js/faker/locale/de_CH');
     case 'en':
-      return import('faker/locale/en');
+      return import('@faker-js/faker/locale/en');
     case 'en_AU':
-      return import('faker/locale/en_AU');
+      return import('@faker-js/faker/locale/en_AU');
     case 'en_AU_ocker':
-      return import('faker/locale/en_AU_ocker');
+      return import('@faker-js/faker/locale/en_AU_ocker');
     case 'en_BORK':
-      return import('faker/locale/en_BORK');
+      return import('@faker-js/faker/locale/en_BORK');
     case 'en_CA':
-      return import('faker/locale/en_CA');
+      return import('@faker-js/faker/locale/en_CA');
     case 'en_GB':
-      return import('faker/locale/en_GB');
+      return import('@faker-js/faker/locale/en_GB');
     case 'en_IE':
-      return import('faker/locale/en_IE');
+      return import('@faker-js/faker/locale/en_IE');
     case 'en_IND':
-      return import('faker/locale/en_IND');
+      return import('@faker-js/faker/locale/en_IND');
     case 'en_US':
-      return import('faker/locale/en_US');
+      return import('@faker-js/faker/locale/en_US');
     case 'en_ZA':
-      return import('faker/locale/en_ZA');
+      return import('@faker-js/faker/locale/en_ZA');
     case 'es':
-      return import('faker/locale/es');
+      return import('@faker-js/faker/locale/es');
     case 'es_MX':
-      return import('faker/locale/es_MX');
+      return import('@faker-js/faker/locale/es_MX');
     case 'fa':
-      return import('faker/locale/fa');
+      return import('@faker-js/faker/locale/fa');
     case 'fi':
-      return import('faker/locale/fi');
+      return import('@faker-js/faker/locale/fi');
     case 'fr':
-      return import('faker/locale/fr');
+      return import('@faker-js/faker/locale/fr');
     case 'fr_CA':
-      return import('faker/locale/fr_CA');
+      return import('@faker-js/faker/locale/fr_CA');
     case 'fr_CH':
-      return import('faker/locale/fr_CH');
+      return import('@faker-js/faker/locale/fr_CH');
     case 'ge':
-      return import('faker/locale/ge');
+      return import('@faker-js/faker/locale/ge');
     case 'id_ID':
-      return import('faker/locale/id_ID');
+      return import('@faker-js/faker/locale/id_ID');
     case 'it':
-      return import('faker/locale/it');
+      return import('@faker-js/faker/locale/it');
     case 'ja':
-      return import('faker/locale/ja');
+      return import('@faker-js/faker/locale/ja');
     case 'ko':
-      return import('faker/locale/ko');
+      return import('@faker-js/faker/locale/ko');
     case 'nb_NO':
-      return import('faker/locale/nb_NO');
-    case 'nep':
-      return import('faker/locale/nep');
+      return import('@faker-js/faker/locale/nb_NO');
+    case 'ne':
+      return import('@faker-js/faker/locale/ne');
     case 'nl':
-      return import('faker/locale/nl');
+      return import('@faker-js/faker/locale/nl');
     case 'nl_BE':
-      return import('faker/locale/nl_BE');
+      return import('@faker-js/faker/locale/nl_BE');
     case 'pl':
-      return import('faker/locale/pl');
+      return import('@faker-js/faker/locale/pl');
     case 'pt_BR':
-      return import('faker/locale/pt_BR');
+      return import('@faker-js/faker/locale/pt_BR');
     case 'pt_PT':
-      return import('faker/locale/pt_PT');
+      return import('@faker-js/faker/locale/pt_PT');
     case 'ro':
-      return import('faker/locale/ro');
+      return import('@faker-js/faker/locale/ro');
     case 'ru':
-      return import('faker/locale/ru');
+      return import('@faker-js/faker/locale/ru');
     case 'sk':
-      return import('faker/locale/sk');
+      return import('@faker-js/faker/locale/sk');
     case 'sv':
-      return import('faker/locale/sv');
+      return import('@faker-js/faker/locale/sv');
     case 'tr':
-      return import('faker/locale/tr');
+      return import('@faker-js/faker/locale/tr');
     case 'uk':
-      return import('faker/locale/uk');
+      return import('@faker-js/faker/locale/uk');
     case 'vi':
-      return import('faker/locale/vi');
+      return import('@faker-js/faker/locale/vi');
     case 'zh_CN':
-      return import('faker/locale/zh_CN');
+      return import('@faker-js/faker/locale/zh_CN');
     case 'zh_TW':
-      return import('faker/locale/zh_TW');
+      return import('@faker-js/faker/locale/zh_TW');
     default:
-      return import('faker/locale/en');
+      return import('@faker-js/faker/locale/en');
   }
 }
